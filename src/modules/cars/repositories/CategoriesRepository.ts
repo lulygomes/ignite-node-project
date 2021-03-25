064533903066e4940f8d040e8d9eed3e682f77fb
Luiz Silva <luiz.gsilva42@gmail.com>
2021-03-24 22:42:15 -0300

    reoganização

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../Model/Category";
+import { Category } from "../model/Category";
 import {
   ICategoriesRepository,
   ICreateCategoryDTO,
--- src/modules/cars/repositories/ICategoriesRepository.ts
+++ src/modules/cars/repositories/ICategoriesRepository.ts
@@ -1,4 +1,4 @@
-import { Category } from "../Model/Category";
+import { Category } from "../model/Category";
 
 interface ICreateCategoryDTO {
   name: string;
