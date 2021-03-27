33605bc1037c90e519bf4b6fa4fde429b30c3a65
Luiz Silva <luiz.gsilva42@gmail.com>
2021-03-27 19:26:42 -0300

    add singleton instance

--- src/modules/cars/repositories/CategoriesRepository.ts
+++ src/modules/cars/repositories/CategoriesRepository.ts
@@ -7,11 +7,19 @@ import {
 class CategoriesRepository implements ICategoriesRepository {
   private categories: Category[];
 
-  constructor() {
+  private static INSTANCE: CategoriesRepository;
+
+  private constructor() {
     this.categories = [];
   }
-  name: string;
-  description: string;
+
+  public static getInstance(): CategoriesRepository {
+    if (!CategoriesRepository.INSTANCE) {
+      CategoriesRepository.INSTANCE = new CategoriesRepository();
+    }
+
+    return CategoriesRepository.INSTANCE;
+  }
 
   create({ name, description }: ICreateCategoryDTO): void {
     const category = new Category();
