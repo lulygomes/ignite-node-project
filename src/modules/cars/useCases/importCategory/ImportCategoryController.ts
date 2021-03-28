a5ca570a22601d9c2e86664e285fd2b306c1e8ae
Luiz Silva <luiz.gsilva42@gmail.com>
2021-03-28 14:19:18 -0300

    add multer

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from "express";
+
+import ImportCategoryUseCase from "./ImportCategoryUseCase";
+
+class ImportCategoryController {
+  constructor(private importCategory: ImportCategoryUseCase) {}
+  handle(request: Request, response: Response): Response {
+    const { file } = request;
+
+    this.importCategory.execute(file);
+
+    return response.send();
+  }
+}
+
+export default ImportCategoryController;
