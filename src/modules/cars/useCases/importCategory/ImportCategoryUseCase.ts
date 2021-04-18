0bf67db1d107b01ea4b622cbf84e9d763f735802
Luiz Silva <luiz.gsilva42@gmail.com>
2021-04-18 15:59:39 -0300

    add docker

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,58 @@
+import csvParse from "csv-parse";
+import fs from "fs";
+
+import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
+
+interface IImportCategory {
+  name: string;
+  description: string;
+}
+
 class ImportCategoryUseCase {
-  execute(file: Express.Multer.File): void {
-    console.log(file);
+  constructor(private categoryRepository: ICategoriesRepository) {}
+
+  loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+    return new Promise((resolve, reject) => {
+      const stream = fs.createReadStream(file.path);
+      const importCategories: IImportCategory[] = [];
+
+      const parseFile = csvParse();
+
+      stream.pipe(parseFile);
+
+      parseFile
+        .on("data", async (line) => {
+          const [name, description] = line;
+
+          importCategories.push({
+            name,
+            description,
+          });
+        })
+        .on("end", () => {
+          resolve(importCategories);
+        })
+        .on("error", (err) => {
+          reject(err);
+        });
+    });
+  }
+
+  async execute(file: Express.Multer.File): Promise<void> {
+    const categories = await this.loadCategories(file);
+
+    categories.forEach(async (category) => {
+      const { name, description } = category;
+
+      const categoryExist = this.categoryRepository.findByName(name);
+
+      if (!categoryExist) {
+        this.categoryRepository.create({
+          name,
+          description,
+        });
+      }
+    });
   }
 }
 
