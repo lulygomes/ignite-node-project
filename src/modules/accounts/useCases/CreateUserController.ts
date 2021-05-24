b3178ca3ab0ce21c627ce08f4ef7abf6f33573c4
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-05-24 20:41:51 -0300

    update user

--- src/modules/accounts/useCases/CreateUserController.ts
+++ src/modules/accounts/useCases/CreateUserController.ts
@@ -5,7 +5,7 @@ import CreateUserUseCase from "./CreateUserUseCase";
 
 class CreateUserController {
   async handle(request: Request, response: Response): Promise<Response> {
-    const { name, email, password, driver_license, username } = request.body;
+    const { name, email, password, driver_license } = request.body;
     const createUserUseCase = container.resolve(CreateUserUseCase);
 
     await createUserUseCase.execute({
@@ -13,7 +13,6 @@ class CreateUserController {
       email,
       password,
       driver_license,
-      username,
     });
 
     return response.status(201).send();
