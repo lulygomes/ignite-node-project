bc6d7659cbb81f00caaba15a261e59216c39c696
Luiz Antonio Gomes da Silva <luiz.gsilva42@gmail.com>
2021-06-06 17:19:40 -0300

    teste Create Category

---
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -0,0 +1,41 @@
+/* eslint-disable no-return-await */
+import { AppError } from "../../../../erros/AppError";
+import CategoriesRepositoryInMemory from "../../repositories/in-memory/CategoriesRepositoryInMemory";
+import CreateCategoryUseCase from "./CreateCategoryUseCase";
+
+let createCategories: CreateCategoryUseCase;
+let categoriesRepositoryInMemory: CategoriesRepositoryInMemory;
+
+describe("Create Category", () => {
+  beforeEach(() => {
+    categoriesRepositoryInMemory = new CategoriesRepositoryInMemory();
+    createCategories = new CreateCategoryUseCase(categoriesRepositoryInMemory);
+  });
+
+  it("Shoud be able to create new category", async () => {
+    const category = {
+      name: "Category Y",
+      description: "All types Y",
+    };
+    await createCategories.execute(category);
+
+    const categoryCreated = await categoriesRepositoryInMemory.findByName(
+      category.name
+    );
+
+    expect(categoryCreated.name).toBe(category.name);
+  });
+
+  it("Shoud not be able to create a new category when the aready exist", async () => {
+    const CreateCategoryDuplicate = async () => {
+      const category = {
+        name: "Category Y",
+        description: "All types Y",
+      };
+      await createCategories.execute(category);
+      return await createCategories.execute(category);
+    };
+
+    expect(CreateCategoryDuplicate).rejects.toBeInstanceOf(AppError);
+  });
+});
